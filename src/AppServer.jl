755378cf282f1787c36b90bfa03ee1d0ec3a315e
Helmut Haensel <helmut.haensel@gmx.de>
2020-10-07 19:26:41 +0200

    allow for explicit channel specification

--- src/AppServer.jl
+++ src/AppServer.jl
@@ -132,7 +132,7 @@ end
 
 
 """
-    downdown(; webserver::Bool = true, websockets::Bool = true) :: ServersCollection
+    down(; webserver::Bool = true, websockets::Bool = true) :: ServersCollection
 
 Shuts down the servers optionally indicating which of the `webserver` and `websockets` servers to be stopped.
 """
